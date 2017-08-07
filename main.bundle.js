7ca66cc85c792ec03bc4d4170e87790cd728d802
maxime pierre <max@webmediasolutionz.com>
2017-08-06 20:46:56 -0400

    Auto-generated commit

---
+++ main.bundle.js
@@ -0,0 +1,158 @@
+webpackJsonp([1],{
+
+/***/ "../../../../../src async recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = "../../../../../src async recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "\n\t\t<div class=\"outer_container\">\n\t\t\t<div class=\"container\">\n<section id=\"content\" role=\"main\">\n\t\t\t<article id=\"post-4\" class=\"post-4 page type-page status-publish hentry\">\n\t\t\t<div>\n\t\t\t\t<h4>our website is</h4>\n\t\t\t\t<h1>coming soon!</h1>\n\t\t\t\t<p>We have been spending long hours in order to launch our new website. Stay tuned!</p>\n\t\t\t</div>\n\n\t\t\t<img class=\"logo\" src=\"http://webmediasolutionz.com/wp-content/themes/comingsoon/images/logo.jpg\">\n\n\t\t\t<div class=\"contact-form\">\n\t\t\t\t<p>Contact us for any inquiries</p>\n\n\t\t\t\t<div role=\"form\" class=\"wpcf7\" id=\"wpcf7-f11-p4-o1\" dir=\"ltr\" lang=\"en-US\">\n<div class=\"screen-reader-response\"></div>\n<form action=\"/#wpcf7-f11-p4-o1\" method=\"post\" class=\"wpcf7-form\" novalidate=\"novalidate\">\n<div style=\"display: none;\">\n<input name=\"_wpcf7\" value=\"11\" type=\"hidden\">\n<input name=\"_wpcf7_version\" value=\"4.4\" type=\"hidden\">\n<input name=\"_wpcf7_locale\" value=\"en_US\" type=\"hidden\">\n<input name=\"_wpcf7_unit_tag\" value=\"wpcf7-f11-p4-o1\" type=\"hidden\">\n<input name=\"_wpnonce\" value=\"f73900742c\" type=\"hidden\">\n</div>\n<table>\n<tbody><tr>\n<td>\n\t    <span class=\"wpcf7-form-control-wrap your-name\"><input name=\"your-name\" value=\"\" size=\"40\" class=\"wpcf7-form-control wpcf7-text wpcf7-validates-as-required\" aria-required=\"true\" aria-invalid=\"false\" placeholder=\"Name\" type=\"text\"></span>\n\t</td>\n</tr>\n<tr>\n<td>\n\t    <span class=\"wpcf7-form-control-wrap your-email\"><input name=\"your-email\" value=\"\" size=\"40\" class=\"wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email\" aria-required=\"true\" aria-invalid=\"false\" placeholder=\"Email\" type=\"email\"></span>\n\t</td>\n</tr>\n<tr>\n<td>\n\t    <span class=\"wpcf7-form-control-wrap your-subject\"><input name=\"your-subject\" value=\"\" size=\"40\" class=\"wpcf7-form-control wpcf7-text\" aria-invalid=\"false\" placeholder=\"Subject\" type=\"text\"></span>\n\t</td>\n</tr>\n<tr>\n<td>\n\t    <span class=\"wpcf7-form-control-wrap your-message\"><textarea name=\"your-message\" cols=\"40\" rows=\"10\" class=\"wpcf7-form-control wpcf7-textarea\" aria-invalid=\"false\" placeholder=\"Message\"></textarea></span>\n\t</td>\n</tr>\n<tr>\n<td>\n\t    <input value=\"Send\" class=\"wpcf7-form-control wpcf7-submit\" type=\"submit\"><img class=\"ajax-loader\" src=\"http://webmediasolutionz.com/wp-content/plugins/contact-form-7/images/ajax-loader.gif\" alt=\"Sending ...\" style=\"visibility: hidden;\">\n\t</td>\n</tr>\n</tbody></table>\n<div class=\"wpcf7-response-output wpcf7-display-none\"></div></form></div>\n\t\t\t</div>\n\t\t</article>\n\t</section>\n\n\t\t\t</div>\n\t\t</div>\n\t\t<footer>\n\t\t\t<div class=\"container\">\n\t\t\t\t<div class=\"copyright\">\n\t\t\t\t\t© 2017 Web Media Solutionz. All Rights Reserved.\t\t\t\t</div>\n\t\t\t</div>\n\t\t</footer>\n\t\t<script type=\"text/javascript\" src=\"http://webmediasolutionz.com/wp-content/themes/comingsoon/javascript/lib/bootstrap.min.js\"></script>\n\t\t<script type=\"text/javascript\" src=\"http://webmediasolutionz.com/wp-content/plugins/contact-form-7/includes/js/jquery.form.min.js?ver=3.51.0-2014.06.20\"></script>\n<script type=\"text/javascript\">\n/* <![CDATA[ */\nvar _wpcf7 = {\"loaderUrl\":\"http:\\/\\/webmediasolutionz.com\\/wp-content\\/plugins\\/contact-form-7\\/images\\/ajax-loader.gif\",\"recaptchaEmpty\":\"Please verify that you are not a robot.\",\"sending\":\"Sending ...\"};\n/* ]]> */\n</script>\n<script type=\"text/javascript\" src=\"http://webmediasolutionz.com/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=4.4\"></script>\n<script type=\"text/javascript\" src=\"http://webmediasolutionz.com/wp-includes/js/wp-embed.min.js?ver=4.4.10\"></script>\n\t\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_0" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__("../../../../../src/app/app.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
