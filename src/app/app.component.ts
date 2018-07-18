adddddbe38f329eb07c86cfba2fa11914ad95a97
Maxime Pierre <mpirre@justenergy.com>
2018-07-17 22:44:09 -0400

    refactoring a few things here and there

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 
-// Environment config
-import { environment } from "../environments/environment";
-
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
-export class AppComponent {
-  private cdnUrl: string = environment.cdnUrl;
-}
+export class AppComponent { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  cdnUrl: 'http://webmediasolutionz.io:1000'
+  production: true
 };
